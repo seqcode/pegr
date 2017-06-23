f0a2ba358d496cf8674f0e9ab27d366489630e1a
ROBERT YAN <rjy5060@rogue.local>
2017-06-23 15:49:30 -0400

    yep site details now displays correctly on shuanline

--- pegr/grails-app/controllers/pegr/YepController.groovy
+++ pegr/grails-app/controllers/pegr/YepController.groovy
@@ -8,7 +8,10 @@ import java.awt.image.BufferedImage
 class YepController {
 	//takes the text from the sampleID.txt and throws the info into a map for the template function
 	def textinfo(id){
-		String filecontents = new File('grails-app/views/yep/'+id+'.txt').text //text to string
+		//File f = new File('FUKETHIS.txt')
+		//f.write("FUKE ME")
+		String filecontents = new File('webapps/pegr/WEB-INF/grails-app/views/yep/'+id+'.txt').text //this line is for shaunline
+		//String filecontents = new File('grails-app/views/yep/'+id+'.txt').text //this line is for lcoal
 		def filearr = filecontents.split('\n') //remove the headers
 		filecontents = filearr[1] //recast into string
 		filearr=filecontents.split('\t') //remove the tabs
@@ -21,7 +24,6 @@ class YepController {
 		wordMap['YSITES']=filearr[6]
 		wordMap['ZSITES']=filearr[7]
 		return wordMap
-
 	}
 	
 

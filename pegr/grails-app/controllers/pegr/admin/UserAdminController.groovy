d69821953083b8664e0cd6b61e5f2e2a6b8de4d6
Pierce Chaffin <pjc5479@051464.local>
2018-07-25 11:29:36 -0400

    Adds GUI and such for fix

--- pegr/grails-app/controllers/pegr/admin/UserAdminController.groovy
+++ pegr/grails-app/controllers/pegr/admin/UserAdminController.groovy
@@ -7,45 +7,45 @@ import pegr.UserException
 
 class UserAdminController {
 	public static AdminCategory category = AdminCategory.OTHER
-	
+
     def userService
-    
+
 	def index(Integer max, Long affiliationId, Long groupId, String isEnabled) {
-		params.max = Math.min(max ?: 25, 100)
-        
+				//Removed max parameter as to allow for all entries to populate datatable
+
         def affiliations = User.where{}.collect { it.affiliation }.unique()
 
-        def users 
+        def users
         def totalCount
-        
-        if (groupId) {
-            def sort = params.sort ? ("user."+params.sort) : "user.fullName"
-            users = UserRoleGroup.createCriteria().list(max: params.max, offset: params.offset, sort: sort, order: params.order) {
-                eq("roleGroup.id", groupId) 
-            }.collect { it.user }
-            
-            totalCount = UserRoleGroup.where { roleGroup.id == groupId }.count()
-        } else {
-            def query
-            if (affiliationId) {
-                query = User.where { affiliation.id == affiliationId }         
-            } else if (isEnabled != null && isEnabled != "") {
-                query = User.where { enabled == isEnabled }
-            } else {
-                query = User.where{}
-            }
-            users = query.list(params)
-            totalCount = query.count()
-        }
 
-		[users: users, 
-         totalCount: totalCount, 
+        // if (groupId) {
+        //     def sort = params.sort ? ("user."+params.sort) : "user.fullName"
+        //     users = UserRoleGroup.createCriteria().list(max: params.max, offset: params.offset, sort: sort, order: params.order) {
+        //         eq("roleGroup.id", groupId)
+        //     }.collect { it.user }
+				//
+        //     totalCount = UserRoleGroup.where { roleGroup.id == groupId }.count()
+        // } else {
+        //     def query
+        //     if (affiliationId) {
+        //         query = User.where { affiliation.id == affiliationId }
+        //     } else if (isEnabled != null && isEnabled != "") {
+        //         query = User.where { enabled == isEnabled }
+        //     } else {
+        //         query = User.where{}
+        //     }
+        //     users = query.list(params)
+        //     totalCount = query.count()
+        // }
+
+				[users: users,
+         totalCount: totalCount,
          affiliations: affiliations,
          affiliationId: affiliationId,
          groupId: groupId,
          isEnabled: isEnabled]
 	}
-    
+
     def search(String username, String email) {
         def users = []
         if (username && username != "") {
@@ -58,22 +58,22 @@ class UserAdminController {
         }
         render(view: "index", model: [users: users])
     }
-    
+
     def edit(Long userId) {
         def user  = User.get(userId)
         if (!user) {
             render(view: "/404")
             return
-        } 
+        }
         [user: user]
     }
-    
+
     def update(Long userId) {
         def user = User.get(userId)
         if (!user) {
             render(view: "/404")
             return
-        } 
+        }
         user.properties = params
         def groups = params.list("groups")
         try {
@@ -83,9 +83,9 @@ class UserAdminController {
             flash.message = e.message
         }
         redirect(action:"index")
-        
-    }	
-    
+
+    }
+
     def createUser(CreateUserCommand cmd) {
         def groupIds = []
         if (params.groupIds) {
@@ -96,7 +96,7 @@ class UserAdminController {
             render(view: "index", model: [users: [], cmd: cmd])
             return
         }
-        try {            
+        try {
             def user = userService.create(cmd.email,
                             groupIds,
                             cmd.sendEmail
@@ -114,7 +114,7 @@ class UserAdminController {
 class CreateUserCommand {
     String email
     Boolean sendEmail
-    
+
     static constraints = {
         email email: true, nullable: false, blank: false
     }

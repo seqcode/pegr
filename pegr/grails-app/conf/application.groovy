1bca8eed619ca5ebcd5fe845659615766bc1abd0
Danying Shao <danying.shao@gmail.com>
2021-05-12 22:21:02 -0400

    Update application.groovy

--- pegr/grails-app/conf/application.groovy
+++ pegr/grails-app/conf/application.groovy
@@ -175,11 +175,3 @@ grails.plugin.springsecurity.interceptUrlMap = [
     [pattern: '/**',             access:  ['isAuthenticated()']] // everything else requires authenticated user
 ]
 
-grails {
-   mail {
-     props = ["mail.smtp.auth":"true",
-              "mail.smtp.socketFactory.port":"465",
-              "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
-              "mail.smtp.socketFactory.fallback":"false"]
-   }
-}

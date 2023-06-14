82b14741323f25ef9ff37b79d6b452d4bf298330
Danying Shao <danying.shao@gmail.com>
2023-06-14 13:11:04 -0400

    export assay as CSV

--- pegr/grails-app/controllers/pegr/admin/AssayAdminController.groovy
+++ pegr/grails-app/controllers/pegr/admin/AssayAdminController.groovy
@@ -171,6 +171,34 @@ class AssayAdminController {
         redirect(action: "index")
     }
     
+    
+    /**
+     * Export CSV 
+     */
+    def exportCsv() {
+        final String filename = 'Assay.csv'
+        def lines = Assay.findAll().collect { [
+            it.id, 
+            it.name?it.name:"", 
+            it.description?it.description:"", 
+        ].join(',') } as List<String>;
+        
+        def outs = response.outputStream
+        
+        response.status = 200
+        response.contentType = "text/csv;charset=UTF-8";
+        response.setHeader "Content-disposition", "attachment; filename=${filename}"
+        
+        outs << "ID, Name, Description\n"
+        lines.each { String line ->
+            outs << "${line}\n"
+        }
+
+        outs.flush()
+        outs.close()
+
+    }
+    
 
     protected void notFound() {
         request.withFormat {
